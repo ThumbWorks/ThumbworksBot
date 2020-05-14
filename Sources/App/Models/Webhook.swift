4c5fc16c585b3c5ac35f78cb040d1599ad99ecb1
Roderic Campbell <roderic@gmail.com>
2020-05-14 12:26:03 -0700

    Use the testable imports when generating mocks

---
+++ Sources/App/Models/Webhook.swift
@@ -0,0 +1,8 @@
+//
+//  Webhook.swift
+//  App
+//
+//  Created by Roderic Campbell on 5/13/20.
+//
+
+import Foundation
--- scripts/generateMocks.sh
+++ scripts/generateMocks.sh
@@ -1,1 +1,1 @@
-./bin/mockolo -s Sources/ -d ./Tests/AppTests/ProtocolMocks.swift
+./bin/mockolo --testable-imports App -s Sources/ -d ./Tests/AppTests/ProtocolMocks.swift
