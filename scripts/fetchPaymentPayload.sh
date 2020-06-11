c7dc4a9d5e71e354c4622b7f128183a27550d24e
Roderic Campbell <roderic@gmail.com>
2020-06-10 17:03:54 -0700

    Add 2 scripts to run for testing locally

---
+++ scripts/fetchPaymentPayload.sh
@@ -0,0 +1,1 @@
+curl -H 'accept: application/json' -H 'accept-language: en-us' -H 'authorization: Bearer c43a4845f57398c59c67af2de3841503407c0e8d2526b4a287b19c6d227e27d0' --compressed 'https://api.freshbooks.com/accounting/account/xazq5/payments/payments/2403183'
---
+++ scripts/newPaymentWebhook.sh
@@ -0,0 +1,1 @@
+curl -X POST -d '{"user_id" : 1, "name" : "payment.create", "object_id" : 2403183, "account_id" : "xazq5"}' http://thumbworksbot.ngrok.io/webhooks/ready -H "Content-Type: application/json"
